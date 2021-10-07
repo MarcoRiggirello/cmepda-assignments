3d047b0f0554e04363b30f2e2943828a05250c1e
Marco <marco.riggirello@libero.it>
2021-10-07 16:29:31 +0200

    Added all mandatory specifications.

--- basic-1/abcounter.py
+++ basic-1/abcounter.py
@@ -1,6 +1,8 @@
 '''Marco Riggirello'''
 
+from time import time
 import argparse
+import matplotlib.pyplot as plt
 
 def counter(string):
     '''Returns the absolute frequency of letters in a string into a dictionary.
@@ -11,9 +13,12 @@ def counter(string):
     return dict(zip(letters, list(map(lstr.count, letters))))
 
 if __name__ == '__main__':
+    start = time()
     DESC = 'Counts the relative frequencies of letters (case unsensitive).'
     parser = argparse.ArgumentParser(prog='ABCounter', description=DESC)
     parser.add_argument('fname', help='The name of the text file to be processed.')
+    parser.add_argument('-p', '--plot', action='store_true',\
+            help='Plot histogram of letters frequency.')
 
     args = parser.parse_args()
     with open(args.fname, 'r', encoding='utf-8') as file:
@@ -27,3 +32,11 @@ if __name__ == '__main__':
     print('--------|-----------')
     for ch in abs_freqs:
         print(f' {ch}      | {100*abs_freqs[ch]/tot:.2f}')
+
+    if args.plot:
+        plt.bar(*zip(*abs_freqs.items()))
+        plt.title(args.fname)
+        plt.show()
+
+    stop = time()
+    print(f'\nTotal elapsed time: {stop-start:.3f}s.')
