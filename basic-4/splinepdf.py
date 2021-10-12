4f38bd6ff3ba7a741db77a30f0a82606c2066ed0
Marco <marco.riggirello@libero.it>
2021-10-12 18:08:33 +0200

    Changes made during videocall.

--- basic-4/splinepdf.py
+++ basic-4/splinepdf.py
@@ -1,20 +1,27 @@
 """ pseudo-random number generator """
 
 import numpy as np
-from scipy.interpolate import InterpolatedUnivariateSpline
+from scipy.interpolate import InterpolatedUnivariateSpline as IUS
 
-class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
+class ProbabilityDensityFunction(IUS):
     def __init__(self, x, y):
         if np.any(y<0):
             raise ValueError("Sampled pdf values must not be less than zero.")
-        _not_normalized_pdf = InterpolatedUnivariateSpline(x, y, ext="zeros")
-        _norm_coeff = _not_normalized_pdf.integral(-np.inf,np.inf)
-        InterpolatedUnivariateSpline.__init__(self, x, y/_norm_coeff, ext="zeros")
+        not_normalized_pdf = IUS(x, y, ext="zeros")
+        norm_coeff = not_normalized_pdf.integral(-np.inf,np.inf)
+        IUS.__init__(self, x, y/norm_coeff, ext="zeros")
         self.cdf = self.antiderivative()
+        ppf_mask = np.concatenate(([True], np.diff(self.cdf(x))>0.0))
+        self.ppf = IUS(self.cdf(x[ppf_mask]), x[ppf_mask], ext="zeros")
 
     def probability(self, a, b):
         """Tells the probability to find a value between a and b.
         """
         return self.integral(a,b)
 
-
+    def random(self, **kwargs):
+        """ Same as numpy.Generator.random() but random numbers
+        are distributed according to ProbabilityDensityFunction.
+        """
+        rng = np.random.default_rng()
+        return self.ppf(rng.random(**kwargs))
